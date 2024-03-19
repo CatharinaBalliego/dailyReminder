bc69260e3f4fd23364da35e06cba86e6247c1a22
Catharina <catharina_@hotmail.com.br>
2024-03-19 15:58:44 -0300

    modela usuario

---
+++ src/main/java/com/reminder/daily/usuario/domain/Usuario.java
@@ -0,0 +1,24 @@
+package com.reminder.daily.usuario.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import javax.validation.constraints.Email;
+
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@ToString
+@Document(collection = "Usuario")
+public class Usuario {
+    @Id
+    private UUID idUsuario;
+    @Email
+    @Indexed(unique = true)
+    private String email;
+
+}
